5af6acce7d733c3e72c7a7e1d791129f2387924b
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2022-11-19 23:53:36 +0530

    after adding react builpack

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname + "/client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
